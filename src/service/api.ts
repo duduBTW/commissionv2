d4e558bddb441260fd0b55255d168dbd12baf46e
dudubtw <carloseduardo108090@hotmail.com>
2022-11-20 14:51:58 -0300

    removins placeholder custom header

--- src/service/api.ts
+++ src/service/api.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: env.NEXT_PUBLIC_API_URL,
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
 });
 
 export default api;
